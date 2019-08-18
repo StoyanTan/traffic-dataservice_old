d6c2593c2eb4589282494407ece9e6f0d1f41e83
stoyan.tanchev <stoyan.tanchev90@gmail.com>
2019-08-18 16:32:58 +0200

    initial commit

---
+++ src/main/java/com/kaggle/traffic/trafficdataservice/TrafficDataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kaggle.traffic.trafficdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficDataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficDataserviceApplication.class, args);
+	}
+
+}
